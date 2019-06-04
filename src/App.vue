b525f876be976aef97e51549ee3450f9560a54f6
IDCjaja <838927992@qq.com>
2019-06-04 18:29:39 +0800

    Git init project

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <base-map />
+  </div>
+</template>
+
+<script>
+import BaseMap from "./components/base_map.vue"
+
+export default {
+  name: "app",
+  components: {
+    "base-map": BaseMap
+  }
+};
+</script>
+
+<style>
+#app {
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,11 @@
+import Vue from 'vue'
+import App from './App.vue'
+
+Vue.config.productionTip = false
+
+// 导入组件库
+import './styles/index.scss';
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
