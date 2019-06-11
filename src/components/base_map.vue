658b9b7be6a5d643ca05c634e1d0bbe8656ffbfd
IDCjaja <838927992@qq.com>
2019-06-11 10:00:19 +0800

    Render map

---
+++ src/components/base_map.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="container" :style="{ backgroundImage: 'url(' + profile.parameter.background + ')' }">
+    <el-map
+      :accessToken="accessToken"
+      :zoom="profile.parameter.zoom"
+      :center="profile.parameter.center"
+      :mapStyle="profile.parameter.style"
+      @load="onMapLoaded">
+    </el-map>
+  </div>
+</template>
+
+<script>
+import { MglMap } from "vue-mapbox";
+import PROFILE from '../resources/profile.vue';
+
+export default {
+  name: "BaseMap",
+  components: {
+    "el-map": MglMap,
+  },
+  data() {
+    return {
+      profile: PROFILE,
+      map: null,
+      accessToken: "pk.eyJ1IjoiYmlnZGF0YWNkIiwiYSI6ImNqbjFkcW00ZTI4cGszd3J1Njk2aDg5Z2gifQ.0WBA8a87guYK9b4Tf3je5A",
+      regionsOptions: null
+    };
+  },
+
+  created() {
+    this.parseRegionsLayers();
+  },
+
+  methods: {
+    onMapLoaded(event) {
+      this.map = event.map;
+    },
+    parseRegionsLayers() {
+      this.regionsOptions = this.profile.parameter.layers.regions
+    }
+  }
+};
+</script>
+
+<style>
+  .container {
+    width: 100%;
+    height: 100%;
+    position: fixed;
+    z-index: 1;
+    background-size: 100% 100%;
+  }
+
+  .container::before {
+    content: "";
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    z-index: 2;
+    background: #333;
+    opacity: .3;
+  }
+
+  .mgl-map-wrapper {
+    z-index: 4;
+  }
+</style>
+
---
+++ src/resources/profile.vue
@@ -0,0 +1,124 @@
+<script>
+export default {
+  parameter: {
+    center: [104.0723, 30.602836],
+    style: "mapbox://styles/bigdatacd/cjwkdexlu0a6a1cmn15f93irz/draft",
+    zoom: 5,
+    background: "https://apod.nasa.gov/apod/image/1804/MilkyVlei_Liebermann_4000.jpg",
+    layers: {
+      marker: {
+        name: "marker",
+        data: [
+          {
+            properties: {
+              message: "Foo",
+              iconSize: [60, 60]
+            },
+            geometry: {
+              type: "Point",
+              coordinates: [104.223, 30.782836]
+            }
+          },
+          {
+            properties: {
+              message: "Bar",
+              iconSize: [50, 50]
+            },
+            geometry: {
+              type: "Point",
+              coordinates: [104.1182, 30.692846]
+            }
+          },
+          {
+            properties: {
+              message: "Baz",
+              iconSize: [40, 40]
+            },
+            geometry: {
+              type: "Point",
+              coordinates: [104.1753, 30.63263]
+            }
+          },
+          {
+            properties: {
+              message: "lili",
+              iconSize: [40, 40]
+            },
+            geometry: {
+              type: "Point",
+              coordinates: [104.1073, 30.608766]
+            }
+          }
+        ],
+        style: {
+          img: "url",
+          colors: ["#ce2c69", "#ffffff"],
+          size: 100,
+          type: "",
+          animation: "wave"
+        }
+      },
+      clusters: {
+        name: "clusters",
+        data: "https://docs.mapbox.com/mapbox-gl-js/assets/earthquakes.geojson"
+      },
+      regions: {
+        name: "regions",
+        data:
+          "https://raw.githubusercontent.com/ufoe/d3js-geojson/master/china/china.json",
+        elements: {
+          outline: {
+            name: "china",
+            style: {
+              type: "line",
+              color: "#006d2c",
+              width: 2,
+              opacity: 1,
+              dasharray: [2, 2]
+            },
+            events: {
+              "click": "hightlight"
+            }
+          },
+          background: {
+            name: "chinaFill",
+            style: {
+              color: "#006d2c",
+              opacity: 0.3,
+            },
+            events: {
+              "click": "hightlight"
+            }
+          },
+          highlight: {
+            outline: {
+              name: "highlightLine",
+              style: {
+                type: "line",
+                color: "burlywood",
+                width: 2,
+                opacity: 1,
+                dasharray: [2, 2]
+              },
+              events: {
+                "click": "hightlight"
+              }
+            },
+            background: {
+              name: "highlight",
+              style: {
+                color: "aqua",
+                opacity: 0.3,
+              },
+              events: {
+                "click": "hightlight"
+              }
+            },
+          }
+        }
+      }
+    }
+  },
+  out_variable: ["_$currentMarker", "_$currentCluster"]
+};
+</script>
