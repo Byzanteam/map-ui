08e6e881c2eb77b6c96d69069da48770340eff3f
IDCjaja <838927992@qq.com>
2019-06-11 14:37:21 +0800

    Create icon component

---
+++ src/components/icon.vue
@@ -0,0 +1,32 @@
+<template>
+  <svg
+    :width="size + 'px'"
+    :height="size + 'px'"
+    :fill="color">
+    <circle :cx="size/2" :cy="size/2" :r="size/2"/>
+  </svg>
+</template>
+
+<script>
+export default {
+  name: "Icon",
+  props: {
+    size: {
+      type: Number,
+      default () {
+        return "10px";
+      },
+    },
+    color: {
+      type: String,
+      default () {
+        return "#fff";
+      },
+    },
+  }
+}
+</script>
+
+<style>
+
+</style>
