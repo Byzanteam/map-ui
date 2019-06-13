bdffd8be771b68c62a04f34926440557f4e060cd
IDCjaja <838927992@qq.com>
2019-06-13 14:56:14 +0800

    Render cluster

--- src/components/cluster.vue
+++ src/components/cluster.vue
@@ -1,6 +1,17 @@
 <template>
   <div>
-
+    <MglGeojsonLayer
+      :sourceId="clusterOptions.name"
+      layerId="cluster"
+      :layer="geoJsonlayer" />
+    <MglGeojsonLayer
+      :sourceId="clusterOptions.name"
+      layerId="clusterCount"
+      :layer="clusterCountLayer" />
+    <MglGeojsonLayer
+      :sourceId="clusterOptions.name"
+      layerId="unclusteredPoint"
+      :layer="unclusterCountLayer" />
   </div>
 </template>
 
@@ -22,8 +33,18 @@ export default {
       default: null
     },
   },
+  data () {
+    return {
+      geoJsonlayer: null,
+      clusterCountLayer: null,
+      unclusterCountLayer: null
+    }
+  },
   created () {
     this.addSource();
+    this.drawGeoJsonlayer();
+    this.drawClusterCountLayer();
+    this.drawUnclusterCountLayer();
   },
   methods: {
     addSource () {
@@ -35,6 +56,58 @@ export default {
         clusterRadius: 50
       })
     },
+    drawGeoJsonlayer () {
+      this.geoJsonlayer =
+      {
+        id: "clusters",
+        type: "circle",
+        filter: ["has", "point_count"],
+        paint: {
+          "circle-color": [
+            "step",
+            ["get", "point_count"],
+            "#51bbd6",
+            100,
+            "#f1f075",
+            750,
+            "#f28cb1"
+          ],
+          "circle-radius": [
+            "step",
+            ["get", "point_count"],
+            20,
+            100,
+            30,
+            750,
+            40
+          ]
+        }
+      }
+    },
+    drawClusterCountLayer () {
+      this.clusterCountLayer = {
+        id: "clusterCount",
+        type: "symbol",
+        filter: ["has", "point_count"],
+        layout: {
+          "text-field": "{point_count_abbreviated}",
+          "text-font": ["DIN Offc Pro Medium", "Arial Unicode MS Bold"],
+          "text-size": 12
+        }
+      }
+    },
+    drawUnclusterCountLayer () {
+      this.unclusterCountLayer = {
+        type: "circle",
+        filter: ["!", ["has", "point_count"]],
+        paint: {
+          "circle-color": "#11b4da",
+          "circle-radius": 4,
+          "circle-stroke-width": 1,
+          "circle-stroke-color": "#fff"
+        }
+      }
+    }
   }
 }
 </script>
