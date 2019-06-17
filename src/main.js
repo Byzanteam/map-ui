50765e156a44bca2b92c84375b2fcb49c4e96173
IDCjaja <838927992@qq.com>
2019-06-17 14:41:56 +0800

    Delete useless json file

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 // 导入组件库
 import './styles/index.scss';
 
 new Vue({
   render: h => h(App),
-}).$mount('#app')
+}).$mount('#app');
