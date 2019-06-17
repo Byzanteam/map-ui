b8513b850643dbceff030b9c5d7588a31e364dd3
IDCjaja <838927992@qq.com>
2019-06-17 14:15:17 +0800

    Import BaseMap

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,13 @@
 <template>
-  <div id="app"></div>
+  <BaseMap />
 </template>
 
 <script>
+import BaseMap from './components/base_map.vue';
+
 export default {
-  name: "app",
+  components: {
+    BaseMap,
+  }
 };
 </script>
