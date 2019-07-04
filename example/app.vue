7a818ac2af159b10c91590df5c3ae66b5cb97143
moonRider <solar.pom@gmail.com>
2019-07-04 18:34:37 +0800

    chore: refactor example and src
    http://beta.jet.work/projects/a58d0c65-c7fd-41a5-bd28-0198ddc2f8d4/tasks/9a2961a5-6c64-4df8-956f-9390e46a5627

---
+++ example/app.vue
@@ -0,0 +1,23 @@
+<template>
+  <base-map />
+</template>
+
+<script>
+import BaseMap from '../src/components/map.vue';
+
+export default {
+  components: {
+    BaseMap,
+  },
+};
+</script>
+
+<style lang="scss">
+  // move to reset, then let style be scoped
+  html,
+  body {
+    width: 100%;
+    height: 100%;
+    margin: 0;
+  }
+</style>
