b3c76fee4d4b7ed6dfb28cddda61c07c201c031c
hello <hello@hellodeMacBook-Pro.local>
2019-07-05 18:57:25 +0800

    feat: add data

--- example/app.vue
+++ example/app.vue
@@ -19,6 +19,17 @@ export default {
     MapPoint,
   },
 
+  data () {
+    return {
+      pointDatas: [
+        { lnglat: [116.258446, 37.686622], name: '景县' },
+        { lnglat: [113.559954, 22.124049], name: '圣方济各堂区' },
+        { lnglat: [116.366794, 39.915309], name: '西城区' },
+        { lnglat: [116.486409, 39.921489], name: '朝阳区' },
+        { lnglat: [116.286968, 39.863642], name: '丰台区' },
+      ],
+    };
+  },
 };
 </script>
 
