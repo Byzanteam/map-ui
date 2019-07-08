683a4d246ed642f3aca23b9bae14960706751796
idcjaja <838927992@qq.com>
2019-07-08 10:38:03 +0800

    feat: delete regions on app

--- example/app.vue
+++ example/app.vue
@@ -1,20 +1,17 @@
 <template>
   <base-map>
     <test />
-    <regions />
   </base-map>
 </template>
 
 <script>
 import BaseMap from '../src/components/map.vue';
 import test from '../src/components/test.vue';
-import Regions from '../src/components/regions.vue';
 
 export default {
   components: {
     BaseMap,
     test,
-    Regions,
   },
 };
 </script>
