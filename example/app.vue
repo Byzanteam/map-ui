3bb75ad3cfaaa6abb918557422659f66fe716d85
usernameisMan <vip939950559@qq.com>
2019-07-09 14:54:54 +0800

    feat: menu list

--- example/app.vue
+++ example/app.vue
@@ -1,42 +1,35 @@
 <template>
-  <base-map>
-    <map-point
-      :data="pointData"
-      description-key="name"
-    />
-  </base-map>
+  <div class="map">
+    <ul class="menu">
+      <li>
+        <router-link to="/map-point">
+          marker
+        </router-link>
+      </li>
+    </ul>
+    <router-view />
+  </div>
 </template>
 
 <script>
-import BaseMap from '../src/components/map.vue';
-import MapPoint from '../src/components/map_point.vue';
-
 export default {
-  components: {
-    BaseMap,
-    MapPoint,
-  },
 
-  data () {
-    return {
-      pointData: [
-        { lnglat: [116.258446, 37.686622], name: '景县' },
-        { lnglat: [113.559954, 22.124049], name: '圣方济各堂区' },
-        { lnglat: [116.366794, 39.915309], name: '西城区' },
-        { lnglat: [116.486409, 39.921489], name: '朝阳区' },
-        { lnglat: [116.286968, 39.863642], name: '丰台区' },
-      ],
-    };
-  },
 };
 </script>
 
 <style lang="scss">
   // move to reset, then let style be scoped
   html,
-  body {
+  body,
+  .map {
     width: 100%;
     height: 100%;
     margin: 0;
+  }
+
+  .menu {
+    position: absolute;
+    z-index: 9;
+    background: #4196ff;
   }
 </style>
