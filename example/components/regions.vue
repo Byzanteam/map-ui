37369e72b9e49cf3d91694c22a5e7b19c24be732
idcjaja <838927992@qq.com>
2019-07-10 15:18:35 +0800

    feat: delete source

--- example/components/regions.vue
+++ example/components/regions.vue
@@ -5,7 +5,6 @@
   >
     <regions
       :label-data="labelData"
-      :geo-json="geoJson"
       :custom-area="customArea"
     />
   </base-map>
@@ -15,9 +14,68 @@
 import BaseMap from '../../src/components/map.vue';
 import Regions from '../../src/components/regions.vue';
 
-const LABEL_DATA = require('../../source/label.json');
-const GEOJSON = require('../../source/regions.json');
-const CUSTOM_AREA = require('../../source/custom_area.json');
+const LABEL_DATA = [{
+  position: ['113.280637', '23.125178'],
+  zooms: [5, 13],
+  zIndex: 1,
+  opacity: 1,
+  text: {
+    content: '广东',
+    direction: 'center',
+    offset: [0, 0],
+    zooms: [3, 20],
+    style: {
+      fontSize: 15,
+      fontWeight: 'normal',
+      fillColor: '#eee',
+      strokeColor: '#c67805',
+      strokeWidth: 2,
+    },
+  },
+}, {
+  position: ['113.665412', '34.757975'],
+  zooms: [5, 13],
+  zIndex: 1,
+  opacity: 1,
+  text: {
+    content: '河南',
+    direction: 'center',
+    offset: [0, 20],
+    zooms: [3, 20],
+    style: {
+      fontSize: 15,
+      fontWeight: 'normal',
+      fillColor: '#eee',
+      strokeColor: '#c67805',
+      strokeWidth: 2,
+    },
+  },
+}];
+const CUSTOM_AREA = [
+  {
+    name: '西北',
+    codes: ['610000', '620000', '630000', '640000', '650000'],
+    options: {
+      fillColor: '#000000',
+      fillOpacity: 0.7,
+    },
+  },
+  {
+    name: '西藏',
+    codes: ['540000'],
+    options: {
+      fillColor: '#ffffff',
+      fillOpacity: 0.1,
+    },
+  },
+  {
+    name: '华北',
+    codes: ['110000', '120000', '130000', '140000', '150000'],
+    options: {
+      fillColor: '#000000',
+      fillOpacity: 0.3,
+    },
+  }];
 
 export default {
   components: {
@@ -27,7 +85,6 @@ export default {
 
   created () {
     this.labelData = LABEL_DATA;
-    this.geoJson = GEOJSON;
     this.customArea = CUSTOM_AREA;
   },
 };
