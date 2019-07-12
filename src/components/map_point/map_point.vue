8712c228ec1597c178cab8264e5cc52a08e75a25
usernameisMan <vip939950559@qq.com>
2019-07-12 18:27:33 +0800

    feat: marker add option

--- src/components/map_point/map_point.vue
+++ src/components/map_point/map_point.vue
@@ -2,12 +2,39 @@
 import _ from 'lodash';
 import markers from './markers.json';
 
+const { icons: [{ icons: ICONS }], size: [SIZE] } = markers;
+
 const DEFAULT_MAERKER = {
   radius: 20,
   strokeColor: '#767676',
   strokeWidth: 1,
   fill: '#4e6398',
 };
+
+const DEFAULT_CLUSTER = {
+  radius: 30,
+  strokeColor: '#767676',
+  strokeWidth: 1,
+  fill: '#4e6398',
+};
+
+const DEFAULT_MAERKER_LABEL = {
+  fontSize: '12px',
+  color: '#888',
+  fontWeight: 400,
+};
+
+const DEFAULT_MAERKER_VALUE = [
+  { value: 3, style: { fill: '#E28E49' } },
+  { value: 2, style: { fill: '#EAC438' } },
+  { value: 1, style: { fill: '#A6B76F' } },
+];
+
+const DEFAULT_CLUSTER_VALUE = [
+  { value: 3, style: { fill: '#E28E49' } },
+  { value: 2, style: { fill: '#EAC438' } },
+  { value: 1, style: { fill: '#A6B76F' } },
+];
 
 const POINT_TYPES = [
   'circle',
@@ -18,11 +45,7 @@ const POINT_TYPES = [
   'pentagram',
 ];
 
-const LABEL_STYLE = {
-  fontSize: '12px',
-  color: '#fff',
-  fontWeight: 400,
-};
+const DEFAULT_ICON = 'circle';
 
 export const MapPoint = {
   props: {
@@ -30,38 +53,24 @@ export const MapPoint = {
       type: Array,
       default: () => [],
     },
+    noCluster: {
+      type: Boolean,
+    },
     markerStyle: {
       type: Object,
       default: () => ({}),
     },
-    type: {
-      type: String,
-      default: 'circle',
-      validator: value => POINT_TYPES.includes(value),
-    },
-    cluster: {
-      type: Boolean,
-      default: true,
-    },
-    descriptionKey: {
-      type: String,
-      default: 'description',
-    },
-    valueKey: {
-      type: String,
-      default: 'value',
+    clusterStyle: {
+      type: Object,
+      default: () => ({}),
     },
     valueOption: {
-      type: Array,
-      default: () => [],
+      type: Object,
+      default: () => ({}),
     },
     labelKey: {
       type: String,
       default: 'label',
-    },
-    labelStyle: {
-      type: Object,
-      default: () => {},
     },
   },
 
@@ -79,16 +88,61 @@ export const MapPoint = {
     },
 
     markerResult () {
+      const {
+        marker = {},
+        label = {},
+        icon = DEFAULT_ICON,
+      } = this.markerStyle;
+
       return {
-        ...DEFAULT_MAERKER,
-        ...this.markerStyle,
+        pointStyle: {
+          ...DEFAULT_MAERKER,
+          ...marker,
+        },
+        labelStyle: {
+          ...DEFAULT_MAERKER_LABEL,
+          ...label,
+        },
+        icon,
       };
     },
 
-    labelResult () {
+    clusterResult () {
+      const {
+        cluster = {},
+        label = {},
+        icon = DEFAULT_ICON,
+      } = this.clusterStyle;
+
       return {
-        ...LABEL_STYLE,
-        ...this.labelStyle,
+        pointStyle: {
+          ...DEFAULT_CLUSTER,
+          ...cluster,
+        },
+        labelStyle: {
+          ...DEFAULT_MAERKER_LABEL,
+          ...label,
+        },
+        icon,
+      };
+    },
+
+    levelOptionResult () {
+      const {
+        marker = [],
+        cluster = [],
+        valueKey = 'value',
+      } = this.valueOption;
+      return {
+        markerLv: _.sortedUniqBy([
+          ...DEFAULT_MAERKER_VALUE,
+          ...marker,
+        ], obj => obj.value),
+        clusterLv: _.sortedUniqBy([
+          ...DEFAULT_CLUSTER_VALUE,
+          ...cluster,
+        ], obj => obj.value),
+        valueKey,
       };
     },
   },
@@ -102,7 +156,7 @@ export const MapPoint = {
   methods: {
     initMakers (map) {
       this.generateMakers();
-      if (this.cluster) {
+      if (!this.noCluster) {
         this.buildClusterMakers(map);
       } else {
         this.buildMakers(map);
@@ -120,8 +174,19 @@ export const MapPoint = {
       );
     },
 
-    clusterMarker () {
+    clusterMarker ({ count, marker }) {
+      const { clusterLv } = this.levelOptionResult;
 
+      marker.setContent(
+        this.getGraphics(
+          count,
+          this.styleResults(
+            { value: count },
+            clusterLv,
+            this.clusterResult,
+          ),
+        )
+      );
     },
 
     getStringWidth (text, fontSize = 16, fontFamily = 'sans-serif') {
@@ -133,13 +198,27 @@ export const MapPoint = {
     },
 
     generateMakers () {
-      this.generateMarkers = this.data.map((itme) => {
+      const { markerLv, valueKey } = this.levelOptionResult;
+
+      this.generateMarkers = this.data.map((extData) => {
+        const content = this.getGraphics(
+          extData[this.labelKey],
+          this.styleResults(
+            extData,
+            markerLv,
+            this.markerResult,
+            valueKey,
+          )
+        );
+
+        const center = this.markerResult.pointStyle.radius;
+
         const marker = new AMap.Marker({
-          position: itme.lnglat,
-          content: this.getGraphics(itme),
+          position: extData.lnglat,
           anchor: 'bottom-center',
-          extData: itme,
-          offset: new AMap.Pixel(0, 0),
+          extData,
+          offset: new AMap.Pixel(center, center),
+          content,
         });
 
         return marker;
@@ -147,16 +226,18 @@ export const MapPoint = {
     },
 
     buildMakers (map) {
-      _.forEach(this.generateMarkers, value => value.setMap(map));
+      _.forEach(
+        this.generateMarkers,
+        value => value.setMap(map)
+      );
     },
 
-    getLabel (itme, userDiameter) {
-      if (!itme[this.labelKey]) return '';
+    getLabel (label = '', diameter = 0, style = {}) {
+      if (!label) return '';
 
-      const { color, fontWeight, fontSize } = this.labelResult;
-      const label = itme[this.labelKey];
+      const { color, fontWeight, fontSize } = style;
       const fontWidth = this.getStringWidth(label, 12);
-      const fontOffset = -(fontWidth - userDiameter) / 2;
+      const fontOffset = -(fontWidth - diameter) / 2;
 
       const node = `<div
           style="
@@ -164,7 +245,7 @@ export const MapPoint = {
             width:${fontWidth}px;
             height:100%;
             left:${fontOffset}px;
-            line-height:${userDiameter}px;
+            line-height:${diameter}px;
             color:${color};
             font-weight:${fontWeight};
             font-size:${fontSize};
@@ -176,26 +257,29 @@ export const MapPoint = {
       return node;
     },
 
-    getGraphics (itme) {
-      const { icons: [{ icons }], size: [size] } = markers;
+    getGraphics (labelText, style) {
       const {
-        radius,
-        fill,
-        strokeWidth,
-        strokeColor,
-      } = {
-        ...this.markerResult,
-        ...this.levelOptionParse(itme, this.valueKey, this.valueOption),
-      };
+        point: {
+          radius,
+          fill,
+          strokeWidth,
+          strokeColor,
+        },
+        labelStyle,
+        icon,
+      } = style;
 
-      const [path] = icons[this.type].paths;
+      let iconType = DEFAULT_ICON;
+
       // 实际 icon 加上边框大小
-      const viewDiameter = size + strokeWidth * 2;
+      const viewDiameter = SIZE + strokeWidth * 2;
       // 用户设置的 icon 大小
       const userDiameter = radius * 2;
 
+      if (POINT_TYPES.includes(icon)) iconType = icon;
+
       const node = `<div style="width:${userDiameter}px;height:${userDiameter}px">
-        ${this.getLabel(itme, userDiameter, strokeWidth)}
+        ${this.getLabel(labelText, userDiameter, labelStyle)}
         <svg
             viewBox="0 0 ${viewDiameter} ${viewDiameter}"
             width="100%"
@@ -206,16 +290,30 @@ export const MapPoint = {
             stroke="${strokeColor}"
             fill="${fill}"
             transform="translate(${strokeWidth} ${strokeWidth})"
-            d='${path}'>
+            d='${ICONS[iconType].paths}'>
         </svg>
       </div>`;
 
       return node;
     },
 
+    styleResults (point, levelOption, style, vkey = 'value') {
+      const { pointStyle, labelStyle, icon } = style;
+
+      return {
+        point: {
+          ...pointStyle,
+          ...this.levelOptionParse(point, vkey, levelOption),
+        },
+        labelStyle,
+        icon,
+      };
+    },
+
     levelOptionParse (content, key, option) {
       const pointValue = content[key];
       let matchStyle = {};
+
       if (!_.isNumber(pointValue)) return {};
       option.sort((a, b) => a.value - b.value);
 
