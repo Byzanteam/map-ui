107f6740d6567b7daa774646638c54001ab0498d
idcjaja <838927992@qq.com>
2019-07-15 19:52:04 +0800

    feat: add labelMarker component

---
+++ src/components/label_marker.vue
@@ -0,0 +1,27 @@
+<script>
+import _ from 'lodash';
+import MapMixin from '../mixins/map';
+
+export const LabelMarker =  {
+  mixins: [MapMixin],
+
+  props: {
+    labelMarkers: {
+      type: Array,
+      default: () => [],
+    },
+  },
+
+  methods: {
+    mapLoadedFunc () {
+      const layer = new AMap.LabelsLayer();
+      this.map.add(layer);
+      _.each(this.labelMarkers, (label) => {
+        layer.add(new AMap.LabelMarker(label));
+      });
+    },
+  },
+};
+
+export default LabelMarker;
+</script>
