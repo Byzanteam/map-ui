2c74f95168c8dd186ec7b8e88c82c7aad2db5e76
idcjaja <838927992@qq.com>
2019-07-15 20:01:15 +0800

    feat: add labelMarker style default value LABEL_MARKER_STYLE
    http://beta.jet.work/projects/a58d0c65-c7fd-41a5-bd28-0198ddc2f8d4/tasks/ec151136-d6a4-4c21-be24-cbe047cb9f84

--- src/components/label_marker.vue
+++ src/components/label_marker.vue
@@ -2,6 +2,12 @@
 import _ from 'lodash';
 import MapMixin from '../mixins/map';
 
+const LABEL_MARKER_STYLE = {
+  fontSize: 18,
+  fontWeight: 'normal',
+  fillColor: '#fff',
+};
+
 export const LabelMarker =  {
   mixins: [MapMixin],
 
@@ -17,6 +23,10 @@ export const LabelMarker =  {
       const layer = new AMap.LabelsLayer();
       this.map.add(layer);
       _.each(this.labelMarkers, (label) => {
+        label.text.style = {
+          ...LABEL_MARKER_STYLE,
+          ...label.text.style,
+        };
         layer.add(new AMap.LabelMarker(label));
       });
     },
