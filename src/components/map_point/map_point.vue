476f214ff76edf38e6ffbabcf3b1fa0bb126ca92
usernameisMan <vip939950559@qq.com>
2019-07-16 14:54:52 +0800

    feat: generateMakers

--- src/components/map_point/map_point.vue
+++ src/components/map_point/map_point.vue
@@ -77,77 +77,36 @@ export const MapPoint = {
 
   methods: {
     mapLoadedFunc () {
+      this.initMakers(this.instance.map);
+    },
+
+    initMakers (map) {
       this.generateMakers();
-      if (this.cluster) {
-        this.buildClusterMakers(this.map);
-      } else {
-        this.buildMakers(this.map);
-      }
-    },
-
-    buildClusterMakers (map) {
-      map.plugin(
-        ['AMap.MarkerClusterer'],
-        () => new AMap.MarkerClusterer(
-          map,
-          this.generateMarkers,
-          { renderClusterMarker: this.clusterMarker },
-        )
-      );
-    },
-
-    clusterMarker (context) {
-
     },
 
     generateMakers () {
-      this.generateMarkers = this.data.map((itme) => {
+      const { markerLv, valueKey } = this.levelOptionResult;
+      this.generateMarkers = this.data.map((extData) => {
+        const content = this.getGraphics(
+          extData[this.labelKey],
+          this.styleResults(
+            extData,
+            markerLv,
+            this.markerResult,
+            valueKey,
+          ),
+        );
+        const center = this.markerResult.pointStyle.radius;
         const marker = new AMap.Marker({
-          position: itme.lnglat,
-          content: this.getGraphics(),
+          position: extData.lnglat,
           anchor: 'bottom-center',
-          extData: itme,
-          offset: new AMap.Pixel(0, 0),
+          extData,
+          offset: new AMap.Pixel(center, center),
+          content,
         });
-
+        marker.on('click', e => this.$emit('pointClick', e));
         return marker;
       });
-    },
-
-    buildMakers (map) {
-      _.forEach(this.generateMarkers, value => value.setMap(map));
-    },
-
-    getGraphics () {
-      const { icons: [{ icons }], size: [size] } = markers;
-      const {
-        radius,
-        fillColor,
-        strokeWeight,
-        strokeColor,
-      } = this.markerResult;
-
-      const [path] = icons[this.type].paths;
-
-      // 实际 icon 加上边框大小
-      const viewDiameter = size + strokeWeight * 2;
-      // 用户设置的 icon 大小
-      const userDiameter = radius * 2;
-
-      const node = `<svg
-          viewBox="0 0 ${viewDiameter} ${viewDiameter}"
-          width="${userDiameter}px"
-          height="${userDiameter}px"
-        >
-        <path
-          stroke-width="${strokeWeight}"
-          stroke="${strokeColor}"
-          fill="${fillColor}"
-          transform="translate(${strokeWeight} ${strokeWeight})"
-          d='${path}'>
-      </svg>`;
-
-      return node;
     },
   },
 };
