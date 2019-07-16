ed8d280da5496e3e6ed6aec5ee9c32259d6e50c9
moonRider <solar.pom@gmail.com>
2019-07-16 15:08:28 +0800

    feat: render edges and points in airline
    http://beta.jet.work/projects/a58d0c65-c7fd-41a5-bd28-0198ddc2f8d4/tasks/649b592e-2cb1-425a-8ef6-1a74f7c0c10b

---
+++ src/components/airline.vue
@@ -0,0 +1,101 @@
+<script>
+import _ from 'lodash';
+import MapMixin from '../mixins/map';
+
+export const AirLine = {
+  mixins: [MapMixin],
+
+  props: {
+    points: {
+      type: Array,
+      required: true,
+      validator (val) {
+        // 至少保证有2个点
+        return val.length > 1;
+      },
+    },
+    edges: {
+      type: Array,
+      default: () => [],
+      validator (val) {
+        // 边必须有起点(source)和终点(target)信息
+        return _.every(
+          val,
+          edge => (_.hasIn(edge, 'source') && _.hasIn(edge, 'target')),
+        );
+      },
+    },
+    // 需要绘制从这些点出去的所有线
+    outPoints: {
+      type: Array,
+      default: () => [],
+    },
+    // 需要绘制进入这些点的所有边
+    inPoints: {
+      type: Array,
+      default: () => [],
+    },
+  },
+
+  computed: {
+    toBeDrawnEdges () {
+      return _.filter(this.edges, (edge) => {
+        const { source } = edge;
+        return _.findIndex(this.outPoints, point => point.id === source) > -1;
+      });
+    },
+  },
+
+  methods: {
+    mapLoadedFunc () {
+      this.renderPoints();
+      this.renderEdges();
+    },
+
+    renderPoints () {
+      _.forEach(this.points, ({ lng, lat }) => {
+        new AMap.CircleMarker({
+          center: [lng, lat],
+          radius: 10,
+          strokeColor: 'white',
+          strokeWeight: 2,
+          strokeOpacity: 0.5,
+          fillColor: 'rgba(0,0,255,1)',
+          fillOpacity: 0.5,
+          bubble: true,
+          cursor: 'pointer',
+          clickable: true,
+        }).setMap(this.map);
+      });
+    },
+
+    renderEdges () {
+      _.forEach(this.toBeDrawnEdges, (edge) => {
+        const path = this._getPointsByEdge(edge);
+        new AMap.Polyline({
+          path,
+          strokeColor: '#3366FF',
+          strokeOpacity: 1,
+          strokeWeight: 2,
+          // 折线样式还支持 'dashed'
+          strokeStyle: 'solid',
+          lineJoin: 'round',
+          lineCap: 'round',
+        }).setMap(this.map);
+      });
+    },
+
+    _getPointsByEdge (edge) {
+      const { source, target } = edge;
+      const sourcePoint = _.find(this.points, point => point.id === source);
+      const targetPoint = _.find(this.points, point => point.id === target);
+      return [
+        [sourcePoint.lng, sourcePoint.lat],
+        [targetPoint.lng, targetPoint.lat],
+      ];
+    },
+  },
+};
+
+export default AirLine;
+</script>
