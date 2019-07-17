ce5a358de4a2a67efa60ce2dbe47e95f3523cd99
usernameisMan <vip939950559@qq.com>
2019-07-17 11:28:29 +0800

    feat: add base HeatMap

---
+++ src/components/heat_map.vue
@@ -0,0 +1,29 @@
+<script>
+import MapMixin from '../mixins/map';
+
+export default {
+  mixins: [MapMixin],
+
+  props: {
+    heatPoints: {
+      type: Array,
+      default: () => [],
+    },
+  },
+
+  methods: {
+    mapLoadedFunc () {
+      this.createHeatMap();
+    },
+
+    createHeatMap () {
+      this.map.plugin(['AMap.Heatmap'], () => {
+        const heatmap = new AMap.Heatmap(
+          this.map,
+        );
+        heatmap.setDataSet({ data: this.heatPoints });
+      });
+    },
+  },
+};
+</script>
