554533e60ce93b26534695c5aa3299c4b8a6c5d6
usernameisMan <vip939950559@qq.com>
2019-07-17 12:23:07 +0800

    refactor: data to const DATA_TEST

--- example/components/heat_map.vue
+++ example/components/heat_map.vue
@@ -13,6 +13,33 @@
 import HeatMap from '../../src/components/heat_map.vue';
 import BaseMap from '../../src/components/map.vue';
 
+const DATA_TEST = [
+  {
+    id: 66535,
+    info: '北京',
+    location: [116.389275, 39.925818],
+    val: 10,
+  },
+  {
+    id: 31234,
+    info: '北京',
+    location: [116.191031, 39.988585],
+    val: 11,
+  },
+  {
+    id: 66535,
+    info: '北京',
+    location: [116.287444, 39.810742],
+    val: 12,
+  },
+  {
+    id: 31234,
+    info: '北京',
+    location: [116.481707, 39.940089],
+    val: 13,
+  },
+];
+
 export default {
   components: {
     HeatMap,
@@ -22,37 +49,11 @@ export default {
   data () {
     return {
       heatPoints: [],
-      data: [
-        {
-          id: 66535,
-          info: '北京',
-          location: [116.389275, 39.925818],
-          val: 10,
-        },
-        {
-          id: 31234,
-          info: '北京',
-          location: [116.191031, 39.988585],
-          val: 11,
-        },
-        {
-          id: 66535,
-          info: '北京',
-          location: [116.287444, 39.810742],
-          val: 12,
-        },
-        {
-          id: 31234,
-          info: '北京',
-          location: [116.481707, 39.940089],
-          val: 13,
-        },
-      ],
     };
   },
 
   mounted () {
-    this.heatPoints = this.data.map((currentValue) => {
+    this.heatPoints = DATA_TEST.map((currentValue) => {
       const {
         id,
         info,
