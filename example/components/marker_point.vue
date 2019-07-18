04eb283a4abe25b7a4bc6eb91d4fa9cfc7aa9514
usernameisMan <vip939950559@qq.com>
2019-07-18 14:31:30 +0800

    refactor: delete useless data

--- example/components/marker_point.vue
+++ example/components/marker_point.vue
@@ -20,11 +20,11 @@ export default {
   data () {
     return {
       pointData: [
-        { lnglat: [116.258446, 37.686622], description: '景县' },
-        { lnglat: [113.559954, 22.124049], description: '圣方济各堂区' },
-        { lnglat: [116.366794, 39.915309], description: '西城区' },
-        { lnglat: [116.486409, 39.921489], description: '朝阳区' },
-        { lnglat: [116.286968, 39.863642], description: '丰台区' },
+        { lnglat: [116.258446, 37.686622] },
+        { lnglat: [113.559954, 22.124049] },
+        { lnglat: [116.366794, 39.915309] },
+        { lnglat: [116.486409, 39.921489] },
+        { lnglat: [116.286968, 39.863642] },
       ],
     };
   },
