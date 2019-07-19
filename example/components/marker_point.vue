b609f85ccb96a2167844b16f79a80505a36718f0
usernameisMan <vip939950559@qq.com>
2019-07-19 16:02:16 +0800

    refactor: test data under the fixed

--- example/components/marker_point.vue
+++ example/components/marker_point.vue
@@ -24,11 +24,11 @@ export default {
   data () {
     return {
       pointData: [
-        { lnglat: [116.258446, 37.686622], value: 0 },
-        { lnglat: [113.559954, 22.124049], value: 1.5 },
-        { lnglat: [116.366794, 39.915309], value: 2 },
-        { lnglat: [116.486409, 39.921489], value: 2 },
-        { lnglat: [116.286968, 39.863642], value: 5 },
+        { id: 1, location: [116.258446, 37.686622], value: 0 },
+        { id: 2, location: [113.559954, 22.124049], value: 1.5 },
+        { id: 3, location: [116.366794, 39.915309], value: 2 },
+        { id: 4, location: [116.486409, 39.921489], value: 2 },
+        { id: 5, location: [116.286968, 39.863642], value: 5 },
       ],
       markerStyleMap: [
         { value: 1, color: 'green' },
