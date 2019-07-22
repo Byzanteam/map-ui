4eb6f6afd9cd4f2048b50fdd599d7dd514f4970c
usernameisMan <vip939950559@qq.com>
2019-07-22 10:38:22 +0800

    feat: responsive testing + events

--- example/components/marker.vue
+++ example/components/marker.vue
@@ -8,12 +8,45 @@
       :markers="pointData"
       :marker-style-map="markerStyleMap"
       icon="water-droplet"
+      @markerClick="click"
     />
   </base-map>
 </template>
 <script>
 import MarkerPoint from '../../src/components/marker';
 import BaseMap from '../../src/components/map.vue';
+
+const MARKERS = [
+  {
+    id: 1,
+    location: [116.258446, 37.686622],
+    value: 0,
+    label: '一号点',
+  },
+  {
+    id: 2,
+    location: [113.559954, 22.124049],
+    value: 1.5,
+  },
+  {
+    id: 3,
+    location: [116.366794, 39.915309],
+    value: 2,
+    label: '三号点',
+  },
+  {
+    id: 4,
+    location: [116.486409, 39.921489],
+    value: 2,
+    label: '四号点',
+  },
+  {
+    id: 5,
+    location: [116.286968, 39.863642],
+    value: 5,
+    label: '五号点',
+  },
+],
 
 export default {
   components: {
@@ -23,42 +56,22 @@ export default {
 
   data () {
     return {
-      pointData: [
-        {
-          id: 1,
-          location: [116.258446, 37.686622],
-          value: 0,
-          label: '一号点',
-        },
-        {
-          id: 2,
-          location: [113.559954, 22.124049],
-          value: 1.5,
-        },
-        {
-          id: 3,
-          location: [116.366794, 39.915309],
-          value: 2,
-          label: '三号点',
-        },
-        {
-          id: 4,
-          location: [116.486409, 39.921489],
-          value: 2,
-          label: '四号点',
-        },
-        {
-          id: 5,
-          location: [116.286968, 39.863642],
-          value: 5,
-          label: '五号点',
-        },
-      ],
+      pointData: [],
       markerStyleMap: [
         { value: 1, color: 'green' },
         { value: 2, color: 'red' },
       ],
     };
+  },
+
+  mounted () {
+    this.pointData = MARKERS;
+  },
+
+  methods: {
+    click () {
+      this.pointData = [];
+    },
   },
 };
 </script>
