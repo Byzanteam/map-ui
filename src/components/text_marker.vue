01aee9b88bc44248fee6fa5004666b3565e11b50
idcjaja <838927992@qq.com>
2019-07-25 20:33:45 +0800

    fix _renderLabel function

--- src/components/text_marker.vue
+++ src/components/text_marker.vue
@@ -51,17 +51,16 @@ export const TextMarker = {
       this.overlayGroup.clearOverlays();
     },
     _renderLabel () {
-      const texts = _.map(this.texts, (label) => {
-        const text = new AMap.Text({
+      const texts = _.map(this.texts, label => (
+        new AMap.Text({
           text: label.text,
           position: label.position,
           style: {
             ...DEFAULT_STYLE,
             ...this.textStyle,
           },
-        });
-        return text;
-      });
+        })
+      ));
       this._generateOverlayGroup(texts);
     },
     _generateOverlayGroup (texts) {
