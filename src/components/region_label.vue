264377b8026f34e8c6d673e6015df0f2299374a9
idcjaja <838927992@qq.com>
2019-07-25 17:51:13 +0800

    feat: add regionsLabel component

---
+++ src/components/region_label.vue
@@ -0,0 +1,59 @@
+<script>
+import _ from 'lodash';
+import MapMixin from '../mixins/map';
+
+const DEFAULT_STYLE = {
+  border: 'none',
+  'background-color': 'transparent',
+  color: '#fff',
+  'font-weight': 'normal',
+  'font-size': 18,
+};
+
+export const RegionLabel = {
+  mixins: [MapMixin],
+
+  props: {
+    labels: {
+      type: Array,
+      default: () => [],
+    },
+    labelStyle: {
+      type: Object,
+      default: () => ({}),
+    },
+  },
+
+  data () {
+    return {
+      texts: [],
+    };
+  },
+
+  methods: {
+    mapLoadedFunc () {
+      _.each(this.labels, (label) => {
+        const text = new AMap.Text({
+          text: label.text,
+          position: label.position,
+          style: {
+            ...DEFAULT_STYLE,
+            ...this.labelStyle,
+          },
+        });
+        this.texts.push(text);
+        text.setMap(this.map);
+      });
+    },
+
+    clear () {
+      _.each(this.texts, (text) => {
+        text.hide();
+      });
+      this.texts = [];
+    },
+  },
+};
+
+export default RegionLabel;
+</script>
