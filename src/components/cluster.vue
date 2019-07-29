712f28ef43ccdee708d7e7a0b837fc920ba854dc
idcjaja <838927992@qq.com>
2019-07-29 13:59:33 +0800

    feat: get map markers to render cluster

--- src/components/cluster.vue
+++ src/components/cluster.vue
@@ -8,12 +8,30 @@ export default {
   mixins: [MapMixin],
 
   props: {
-
+    markers: {
+      type: Array,
+      default: () => [],
+    },
   },
 
-  created () {
+  methods: {
+    mapLoadedFunc () {
+      this.renderCluster();
+    },
 
+    renderCluster () {
+      const markers = this.map.getAllOverlays('marker');
+      this.map.plugin(['AMap.MarkerClusterer'], () => {
+        const cluster = new AMap.MarkerClusterer(
+          this.map,
+          markers,
+          {
+            gridSize: 80,
+          }
+        );
+        cluster.on('click', e => (this.$emit('clusterClick', e)));
+      });
+    },
   },
-
 };
 </script>
