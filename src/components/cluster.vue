ecf8f6eeb571f13a4484b71bd74cd633dd7427f6
idcjaja <838927992@qq.com>
2019-07-29 10:56:23 +0800

    feat: add cluster component

---
+++ src/components/cluster.vue
@@ -0,0 +1,19 @@
+<script>
+import MapMixin from '../mixins/map';
+import Icons from './marker/icons.json';
+
+export default {
+  name: 'Cluster',
+
+  mixins: [MapMixin],
+
+  props: {
+
+  },
+
+  created () {
+
+  },
+
+};
+</script>
