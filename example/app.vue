50b21ea44118e63d131625f2df645e93e365d145
idcjaja <838927992@qq.com>
2020-06-29 12:24:26 +0800

    feat: add customMarker

--- example/app.vue
+++ example/app.vue
@@ -46,6 +46,11 @@
           simple_marker
         </router-link>
       </li>
+      <li>
+        <router-link to="/custom_marker">
+          custom_marker
+        </router-link>
+      </li>
     </ul>
     <router-view />
   </div>
--- example/router.js
+++ example/router.js
@@ -20,6 +20,7 @@ const router = new VueRouter({
     { path: '/cluster', component: () => import('./components/cluster') },
     { path: '/stratum_marker', component: () => import('./components/stratum_marker') },
     { path: '/simple_marker', component: () => import('./components/simple_marker') },
+    { path: '/custom_marker', component: () => import('./components/custom_marker') },
   ],
 });
 
