bd1555444186cf730e973379b2c85069e8445392
ll790226724 <790226724@qq.com>
2021-04-29 18:03:12 +0800

    fix: 删除smple_marker

--- example/router.js
+++ example/router.js
@@ -22,7 +22,6 @@ const router = new VueRouter({
     { path: '/custom_marker', component: () => import('./components/custom_marker') },
     { path: '/info_window', component: () => import('./components/info_window') },
     { path: '/canvas-layer', component: () => import('./components/canvas_layer') },
-    { path: '/simple_marker', component: () => import('./components/simple_marker') },
     { path: '/3d', component: () => import('./components/object3D') },  ],
 });
 
